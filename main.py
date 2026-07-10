de40fead29f7e7f3064bde77485207d629fba873
abdalftah193 <128583230+abdalftah193@users.noreply.github.com>
2026-07-10 09:45:23 +0300

    Change Telegram bot token
    Updated the Telegram bot token for authentication.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import yt_dlp
 from telegram import Update
 from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
-TOKEN = "8795590461:AAExwG4Sukmf2QT39RmRS7iNNpKYPWTm4vg"
+TOKEN = "8795590461:AAGh3suTy2sM1_Bkw2bM6ZPWjiWRliFk1hA"
 
 DOWNLOAD_DIR = "downloads"
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
